e0ca64e2b693d1a2adec0f21a6f081e6d5eefe7d
nathan <nathavieiragg@outlook.com>
2026-08-12 00:42:10 -0300

    fix: corrigindo o banco

---
+++ js/firebase-keys.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBGHjJEqyBxSUqxJndkVPBr337_NTka7CU",
+    authDomain: "cofre-furado.firebaseapp.com",
+    projectId: "cofre-furado",
+    storageBucket: "cofre-furado.firebasestorage.app",
+    messagingSenderId: "233001414896",
+    appId: "1:233001414896:web:686cc28e29c0a67028d235",
+    measurementId: "G-5Q65PLLQG9"
+};
